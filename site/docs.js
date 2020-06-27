458b79fbc5ea7d7089a6ff1f7d38ddea723c2b7b
Modulo <modzero@protonmail.com>
2020-06-27 15:18:06 -0500

    logo update

--- site/docs.js
+++ site/docs.js
@@ -1,1 +1,1 @@
-document.body.innerHTML += '<header id="htop"><div class="io"><img src="../social-core.png"></div></header>';
+document.body.innerHTML += '<header id="htop"><div class="io"><img src="../msip-core.png"></div></header>';
